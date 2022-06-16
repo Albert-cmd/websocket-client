8fec722a9a84fc509068d60d3b49d4628461be64
albert-cmd <albert1999dual@gmail.com>
2022-06-16 13:43:13 +0200

    creado el grid con las imagenes de muestra para enviar al servidor con aviones reales.

--- script/img_upload.js
+++ script/img_upload.js
@@ -23,15 +23,11 @@ function base64convert(file) {
 
     fileReader.onload = function(fileLoadedEvent) {
         var srcData = fileLoadedEvent.target.result; // <--- data: base64
-
-        var newImage = document.createElement('img');
+        console.log(srcData)
+        var newImage = document.getElementById('unprocessedImg')
         newImage.src = srcData;
 
-        document.getElementById("imgTest").innerHTML = newImage.outerHTML;
-        alert("Converted Base64 version is " + document.getElementById("imgTest").innerHTML);
-        console.log("Converted Base64 version is " + document.getElementById("imgTest").innerHTML);
     }
-    fileReader.readAsDataURL(file);
 }
 
 function saveFileName(selectedFile){
