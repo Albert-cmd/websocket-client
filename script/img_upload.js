4497b9522b4fd114e17bc0feaf2caa5083c92b6e
albert-cmd <albert1999dual@gmail.com>
2022-06-21 18:49:34 +0200

    cliente funcionando se pueden subir imagenes al gird por url, se pueden subir por url local tambien, patnalla de carga de ajax tambien. faltan las cookies del src para tener persistencia en las fotos

--- script/img_upload.js
+++ script/img_upload.js
@@ -1,35 +1,3 @@
-
-function processRealImg() {
-        // recuperamos el nombre del archivo que se ha subido al servidor
-        var filename = getCookie('SelectedFileName')
-        // Variable to hold request
-        var request;
-        // Bind to the submit event of our form
-        $.ajax({
-            type:"GET", // la variable type guarda el tipo de la peticion GET,POST,..
-            data: {filename: filename},
-            url:"get_image.php", //url guarda la ruta hacia donde se hace la peticion
-            success:function(datos){ //success es una funcion que se utiliza si el servidor retorna informacion
-                console.log(datos)
-
-                // RECOGEMOS LA URL DE DONDE HA GUARDADO LA IMAGEN PROCESADA EL SERVIDOR Y LA MOSTRAMOS
-
-            }})
-
-}
-
-function base64convert(file) {
-    var fileReader = new FileReader();
-
-    fileReader.onload = function(fileLoadedEvent) {
-        var srcData = fileLoadedEvent.target.result; // <--- data: base64
-        console.log(srcData)
-        var newImage = document.getElementById('unprocessedImg')
-        newImage.src = srcData;
-
-    }
-}
-
 function saveFileName(selectedFile){
     var files = selectedFile.files;
     var filename = files[0].name
@@ -57,4 +25,68 @@ function getCookie(cname) {
         }
     }
     return "";
+}
+const getBase64StringFromDataURL = (dataURL) =>
+    dataURL.replace('data:', '').replace(/^.+,/, '');
+
+function processSelectedImg(img){
+    // Get the remote image as a Blob with the fetch API
+    fetch(img.src)
+        .then((res) => res.blob())
+        .then((blob) => {
+            // Read the Blob as DataURL using the FileReader API
+            const reader = new FileReader();
+            reader.onloadend = () => {
+                //console.log(reader.result);
+                // Logs data:image/jpeg;base64,wL2dvYWwgbW9yZ...
+                // Convert to Base64 string
+                const base64 = getBase64StringFromDataURL(reader.result);
+                console.log(base64);
+                $('#imgdisplay').hide()
+                $('#spinner-div').show();//cargamos el spinner
+                $.ajax({
+                    headers: {
+                        "accept": "application/json",
+                        "Access-Control-Allow-Origin":"*"
+                    },
+                    contentType: 'application/json; charset=UTF-8',
+                    type: 'post',
+                    url: 'http://192.168.1.72:8080/process_real_img',
+                    data: JSON.stringify( { "img": base64 } ),
+                    success: function( data ){
+                        try{
+
+                            console.log('------------- response text -----------------')
+                            console.log(data)
+                            document.getElementById('imgdisplay').src='data:image/jpeg;base64,'+data;
+
+                        }catch (error){
+                            console.error(error.message)
+                        }
+                    },
+                    complete:function(){
+                        $('#spinner-div').hide();//quitamos el spipnnero porque ha finalizado la ejecucion
+                        $('#imgdisplay').show()
+                    }
+                    ,dataType:"text"
+                });
+
+            };
+            reader.readAsDataURL(blob);
+        });
+}
+
+
+
+function MyPopUpWin(url, width, height) {
+    var leftPosition, topPosition;
+    //Allow for borders.
+    leftPosition = (window.screen.width / 2) - ((width / 2) + 10);
+    //Allow for title and status bars.
+    topPosition = (window.screen.height / 2) - ((height / 2) + 50);
+    //Open the window.
+    window.open(url, "Window2",
+        "status=no,height=" + height + ",width=" + width + ",resizable=yes,left="
+        + leftPosition + ",top=" + topPosition + ",screenX=" + leftPosition + ",screenY="
+        + topPosition + ",toolbar=no,menubar=no,scrollbars=no,location=no,directories=no");
 }
--- upload.php
+++ upload.php
@@ -1,4 +1,5 @@
 <?php
+
 $target_dir = "real_imgs/";
 $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
 $uploadOk = 1;
@@ -13,43 +14,42 @@ if(isset($_POST["submit"])) {
   } else {
     $msg= "File is not an image.";
     $uploadOk = 0;
-    echo "<script type='text/javascript'>alert('$msg');</script>";
+    //echo "<script type='text/javascript'>alert('$msg');</script>";
   }
 }
 // Check if file already exists
 if (file_exists($target_file)) {
   $msg= "Sorry, file already exists.";
-  echo "<script type='text/javascript'>alert('$msg');</script>";
+  //echo "<script type='text/javascript'>alert('$msg');</script>";
   $uploadOk = 1;
 }
 // Check file size
 if ($_FILES["fileToUpload"]["size"] > 500000) {
   $msg= "img size exceded";
-  echo "<script type='text/javascript'>alert('$msg');</script>";
+  //echo "<script type='text/javascript'>alert('$msg');</script>";
   $uploadOk = 0;
 }
 // Allow certain file formats
-if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
-&& $imageFileType != "gif" ) {
-    $msg ="Formatos admitidos: JPG, JPEG, PNG & GIF";
-    echo "<script type='text/javascript'>alert('$msg');</script>";
+if($imageFileType != "jpg" && $imageFileType != "jpeg" ) {
+    $msg ="Formatos admitidos: JPG, JPEG";
+    //echo "<script type='text/javascript'>alert('$msg');</script>";
     $uploadOk = 0;
 }
 // Check if $uploadOk is set to 0 by an error
 if ($uploadOk == 0) {
   $msg = "Sorry, your file was not uploaded.";
-   echo "<script type='text/javascript'>alert('$msg');</script>";
+   //echo "<script type='text/javascript'>alert('$msg');</script>";
 // if everything is ok, try to upload file
 } else {
   if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
     $msg =  "The file ". htmlspecialchars( basename( $_FILES["fileToUpload"]["name"])). " has been uploaded.";
 
+      //  echo "<script type='text/javascript'>alert('$msg');</script>";
         header("Location: /websocket-client/img_processing.html");
-        echo "<script type='text/javascript'>alert('$msg');</script>";
 
   } else {
     $msg =  "error en la subida del archivo.";
-    echo "<script type='text/javascript'>alert('$msg');</script>";
+    //echo "<script type='text/javascript'>alert('$msg');</script>";
   }
 }
 ?>
