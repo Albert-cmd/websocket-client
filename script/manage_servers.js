3db49bb6045b7d87cab54a484ee57d872f3ddea1
albert-cmd <albert1999dual@gmail.com>
2022-07-11 18:05:13 +0200

    añadidos los botones de iniciar servidores y detener servidores, pprogramado y funcionando

--- script/manage_servers.js
+++ script/manage_servers.js
@@ -1,9 +1,10 @@
-socket.connect()
 function stop_instances(){
+    socket.connect()
     console.log("stop_instances clicked")
     socket.emit('stop_aws')
 }
 function start_instances(){
+    socket.connect()
     console.log("start_instances clicked")
     socket.emit('run_aws')
 }
